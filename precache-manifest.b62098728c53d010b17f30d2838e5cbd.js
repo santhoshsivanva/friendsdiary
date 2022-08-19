676289ce03625070b928f81ab912d6c9b71fa57e
Santhosh Sivan <v.sivan777vss@gmail.com>
2022-08-19 10:27:27 +0530

    Updates

---
+++ precache-manifest.b62098728c53d010b17f30d2838e5cbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "771e03c7c18032adacb1",
+    "url": "/friendsdiary/static/css/main.7492cef0.chunk.css"
+  },
+  {
+    "revision": "771e03c7c18032adacb1",
+    "url": "/friendsdiary/static/js/main.8a11839c.chunk.js"
+  },
+  {
+    "revision": "1b5c5cc62379d3fa4d6c",
+    "url": "/friendsdiary/static/js/runtime~main.099e3cd2.js"
+  },
+  {
+    "revision": "7a4aebf18eb80a385031",
+    "url": "/friendsdiary/static/css/2.26983752.chunk.css"
+  },
+  {
+    "revision": "7a4aebf18eb80a385031",
+    "url": "/friendsdiary/static/js/2.bbcf7a41.chunk.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/friendsdiary/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "7e763d3a7a099e57bd0f65cfeadad4ff",
+    "url": "/friendsdiary/index.html"
+  }
+];
